302849df777bb0c31f3ff8d8c82a4c44dcc14b5d
MichaelAns <mi035ms@gmail.com>
2022-05-27 17:48:53 +0300

    Создание контекста базы данных и фабрики контекста

---
+++ Stocks.EntityFramework/Date/StocksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stocks.EntityFramework.Models;
+
+namespace Stocks.EntityFramework.Date
+{
+    internal class StocksDbContext : DbContext
+    {
+        public StocksDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Dividend> Dividend { get; set; }
+        public DbSet<MarketStock> MarketStock { get; set; }
+        public DbSet<Market> Market { get; set; }
+        public DbSet<Issuer> Issuer { get; set; }
+        public DbSet<CostByDate> CostByDate { get; set; }
+    }
+}
---
+++ Stocks.EntityFramework/Date/StocksDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Stocks.EntityFramework.Date
+{
+    internal class StocksDbContextFactory : IDesignTimeDbContextFactory<StocksDbContext>
+    {
+        public StocksDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<StocksDbContext>();
+            options.UseNpgsql("Host=localhost;Port=5432;Database=SimpleTraderDB;Username=postgres;Password=postgres");
+            return new StocksDbContext(options.Options);
+        }
+    }
+}

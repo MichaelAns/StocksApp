dc240ce3b0b961211f7169698877131f53a9b1ff
MichaelAns <mi035ms@gmail.com>
2022-05-27 18:14:20 +0300

    Создание базовых классов Command и ViewModel. создание RelayCommand

---
+++ Stocks.WPF/Infrastructures/Commands/RelayCommand.cs
@@ -0,0 +1,19 @@
+﻿using Stocks.WPF.Infrastructures.Commands.Base;
+using System;
+
+namespace Stocks.WPF.Infrastructures.Commands
+{
+    internal class RelayCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+        public override bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _execute(parameter);
+    }
+}

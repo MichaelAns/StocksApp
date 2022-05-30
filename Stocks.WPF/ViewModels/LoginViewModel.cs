8e7a78186683c5e08e04f760f94667f79dcc5cb7
MichaelAns <mi035ms@gmail.com>
2022-05-30 18:05:21 +0300

    Улучшенная версия входа

--- Stocks.WPF/ViewModels/LoginViewModel.cs
+++ Stocks.WPF/ViewModels/LoginViewModel.cs
@@ -7,7 +7,51 @@ namespace Stocks.WPF.ViewModels
 {
     internal class LoginViewModel : ViewModels.Base.ViewModel
     {
-        //public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand();
-        public ICommand OnButtonClick => OpenViewModel.MainNavigator.UpdateCurrentViewModelCommand;
+        private string _login;
+        private string _password;     
+        public string Login
+        {
+            get => _login;
+            set => Set(ref _login, value);
+        }
+        public string Password
+        {
+            get => _password;
+            set => Set(ref _password, value);
+        }
+        #region Login
+        //проверка на поля
+        private bool CanLogin(object obj)
+        {
+            if (string.IsNullOrEmpty(_login) || string.IsNullOrEmpty(_password))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //сам вход
+        private void LogIn(object obj)
+        {
+            try
+            {
+                //действия
+
+                OpenViewModel.MainNavigator.CurrentViewModel = new MainViewModel();
+            }
+            catch
+            {
+
+            } 
+        }
+        public ICommand OnLoginClick { get; }
+
+        #endregion
+
+        public LoginViewModel()
+        {
+            OnLoginClick = new RelayCommand(LogIn, CanLogin);
+        }
+
     }
 }
